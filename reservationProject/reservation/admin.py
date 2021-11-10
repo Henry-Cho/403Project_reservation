fd1ac2b7f94e94d2a1dd8b4da23fec0b6db725e0
rcoles14 <90427464+rcoles14@users.noreply.github.com>
2021-11-10 15:56:43 -0700

    models and admin interface

--- reservationProject/reservation/admin.py
+++ reservationProject/reservation/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import doctor, patient, appointment_type, appointment
 # Register your models here.
+
+admin.site.register(doctor)
+admin.site.register(patient)
+admin.site.register(appointment_type)
+admin.site.register(appointment)
+
--- reservationProject/reservation/models.py
+++ reservationProject/reservation/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from datetime import datetime, timedelta
 
 # Create your models here.
+
+class patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    byu_id = models.CharField(max_length=9, blank=True)
+    password = models.CharField(max_length=50)
+    patient_email = models.EmailField(max_length=100)
+    patient_phone = models.CharField(max_length=13, blank=True)
+
+    def __str__(self):
+        return (self.full_name)
+
+    @property
+    def full_name(self):
+         return '%s %s' % (self.first_name, self.last_name)
+
+    def save(self):
+        self.first_name = self.first_name.upper()
+        self.last_name = self.last_name.upper()
+        super(patient, self).save()
+
+class doctor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    doctor_email = models.EmailField(max_length=100)
+    doctor_phone = models.CharField(max_length=13, blank=True)
+
+    def __str__(self):
+        return (self.doc_name)
+
+    @property
+    def doc_name(self):
+        return 'Dr. %s' %(self.last_name)
+
+class appointment_type(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return (self.description)
+
+class appointment(models.Model):
+    type = models.ForeignKey(appointment_type, on_delete=models.CASCADE)
+    day_time = models.DateTimeField()
+
+    def __str__(self):
+        return (self.type, self.day_time)
+
